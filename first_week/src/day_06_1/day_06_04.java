043b5b8302fe112db57817258a5ad60ccf2acd7d
py9245 <py9245@naver.com>
2025-09-19 17:55:04 +0900

    update

--- first_week/src/day_06_1/day_06_04.java
+++ first_week/src/day_06_1/day_06_04.java
@@ -4,67 +4,64 @@ import java.io.*;
 import java.util.*;
 
 public class day_06_04 {
-    static int N, answer;
-    static int[][] arr;
-    static int[][] dxy = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}}; // ↘ ↙ ↖ ↗
-    static int startX, startY;
-    static Set<Integer> root;
+	static int N, answer;
+	static int[][] arr;
+	static int[][] dxy = { { 1, 1 }, { 1, -1 }, { -1, -1 }, { -1, 1 } };
+	static int startX, startY;
+	static Set<Integer> root;
 
-    static void dfs(int x, int y, int d, int cnt, int turn) {
-        int nx = x + dxy[d][0];
-        int ny = y + dxy[d][1];
+	static void dfs(int x, int y, int d, int cnt, int turn) {
+		int nx = x + dxy[d][0];
+		int ny = y + dxy[d][1];
 
-        // 맵 밖이면 종료
-        if (nx < 0 || ny < 0 || nx >= N || ny >= N) return;
+		if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+			return;
 
-        // 시작점으로 돌아왔을 때 (사각형 완성 조건)
-        if (nx == startX && ny == startY && cnt >= 4 && turn == 3) {
-            answer = Math.max(answer, cnt);
-            return;
-        }
+		if (nx == startX && ny == startY && cnt >= 4 && turn == 3) {
+			answer = Math.max(answer, cnt);
+			return;
+		}
 
-        int dessert = arr[nx][ny];
-        if (root.contains(dessert)) return; // 이미 먹은 디저트면 종료
+		int dessert = arr[nx][ny];
+		if (root.contains(dessert))
+			return; // 이미 먹은 디저트면 종료
 
-        root.add(dessert);
+		root.add(dessert);
 
-        // 같은 방향으로 이동
-        dfs(nx, ny, d, cnt + 1, turn);
+		dfs(nx, ny, d, cnt + 1, turn);
 
-        // 방향 바꾸기 (최대 3번까지)
-        if (d < 3 && turn < 3) {
-            dfs(nx, ny, d + 1, cnt + 1, turn + 1);
-        }
+		if (d < 3 && turn < 3) {
+			dfs(nx, ny, d + 1, cnt + 1, turn + 1);
+		}
 
-        root.remove(dessert);
-    }
+		root.remove(dessert);
+	}
 
-    public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("res/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            arr = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < N; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
 
-            answer = -1;
-            // 맨 끝은 사각형을 만들 수 없으므로 0~N-2까지만 탐색
-            for (int i = 0; i < N - 2; i++) {
-                for (int j = 1; j < N - 1; j++) {
-                    startX = i;
-                    startY = j;
-                    root = new HashSet<>();
-                    root.add(arr[i][j]);
-                    dfs(i, j, 0, 1, 0);
-                }
-            }
-            System.out.println("#" + tc + " " + answer);
-        }
-    }
+			answer = -1;
+			for (int i = 0; i < N - 2; i++) {
+				for (int j = 1; j < N - 1; j++) {
+					startX = i;
+					startY = j;
+					root = new HashSet<>();
+					root.add(arr[i][j]);
+					dfs(i, j, 0, 1, 0);
+				}
+			}
+			System.out.println("#" + tc + " " + answer);
+		}
+	}
 }
---
+++ first_week/src/day_07/day_07_01.java
@@ -0,0 +1,109 @@
+package day_07;
+
+import java.io.*;
+import java.util.*;
+
+public class day_07_01 {
+    
+    static final int LIMIT = 2000;
+    static final int[][] DXY = {{0,1},{0,-1},{-1,0},{1,0}};
+    
+    static class Microbe {
+        int x, y, d, k;
+        
+        Microbe(int x, int y, int d, int k) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+            this.k = k;
+        }
+        
+        void move() {
+            x += DXY[d][0];
+            y += DXY[d][1];
+        }
+        
+        boolean isOutOfBounds() {
+            return x < -LIMIT || x > LIMIT || y < -LIMIT || y > LIMIT;
+        }
+        
+        long getPositionKey() {
+            return ((long)(x + LIMIT) << 16) | (y + LIMIT);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            
+            List<Microbe> microbes = new ArrayList<>(N);
+            
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken()) * 2;
+                int y = Integer.parseInt(st.nextToken()) * 2;
+                int d = Integer.parseInt(st.nextToken());
+                int k = Integer.parseInt(st.nextToken());
+                microbes.add(new Microbe(x, y, d, k));
+            }
+            
+            long answer = 0;
+            
+            for (int step = 0; step <= 4000 && microbes.size() > 1; step++) {
+                // 이동 단계
+                for (int i = microbes.size() - 1; i >= 0; i--) {
+                    Microbe m = microbes.get(i);
+                    m.move();
+                    
+                    // 경계 밖으로 나간 미생물 제거
+                    if (m.isOutOfBounds()) {
+                        microbes.remove(i);
+                    }
+                }
+                
+                if (microbes.size() <= 1) break;
+                
+                // 충돌 처리 - 위치별 그룹화
+                microbes.sort((a, b) -> Long.compare(a.getPositionKey(), b.getPositionKey()));
+                
+                List<Microbe> survivors = new ArrayList<>();
+                int i = 0;
+                
+                while (i < microbes.size()) {
+                    long currentPos = microbes.get(i).getPositionKey();
+                    int groupStart = i;
+                    int totalK = 0;
+                    int maxK = 0;
+                    int maxKIdx = i;
+                    
+                    while (i < microbes.size() && microbes.get(i).getPositionKey() == currentPos) {
+                        Microbe m = microbes.get(i);
+                        totalK += m.k;
+                        if (m.k > maxK) {
+                            maxK = m.k;
+                            maxKIdx = i;
+                        }
+                        i++;
+                    }
+                    
+                    // 충돌 처리
+                    if (i - groupStart >= 2) {
+                        // 2개 이상이면 충돌
+                        answer += totalK;
+                    } else {
+                        // 1개면 살아남음
+                        survivors.add(microbes.get(groupStart));
+                    }
+                }
+                
+                microbes = survivors;
+            }
+            
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
+}
